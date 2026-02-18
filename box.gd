4b242e7227ad17f9191fdae5e52580346e4f8d26
chonk potato <thechonkpotato@outlook.com>
2026-02-18 09:56:29 -0500

    feat: Add block collisions
    Add area trigger
    Add meta saving side
    Add music file

---
+++ box.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@export var speed: float = 100.0
+
+var x_range := 0.0
+var y_range := 0.0
+var y_size := 0.0
+@export var padding := 80.0
+
+func _ready(): 
+	x_range = get_viewport_rect().size.x
+	y_range = get_viewport_rect().size.y
+	y_size = $CollisionShape2D.shape.size.y * global_scale.y
+	
+	if randf() < 0.5: 
+		global_position.x = padding
+		set_meta('side', 'left')
+	else: 
+		global_position.x = x_range - padding
+		set_meta('side', 'right')
+	
+	global_position.y = -y_size
+
+func _physics_process(delta: float) -> void:
+	global_position.y += speed * delta
+	if global_position.y > y_range + y_size: queue_free()
--- input_manager.gd
+++ input_manager.gd
@@ -1,7 +1,27 @@
 extends Node2D
 
-func _process(_delta):
-	if Input.is_action_just_pressed('left'):
+@onready var block_cluster = $Blocks
+@onready var cam = $Camera2D
+
+const block_scene: PackedScene = preload('res://box.tscn')
+
+func _ready(): 
+	cam.position.x = cam.get_viewport_rect().size.x / 2
+	cam.position.y = cam.get_viewport_rect().size.y / 2
+	
+	randomize()
+
+func _physics_process(_delta):
+	if Input.is_action_just_pressed('left'): 
 		print('left')
-	if Input.is_action_just_pressed('right'):
+	if Input.is_action_just_pressed('right'): 
 		print('right')
+
+func _on_timer_timeout():
+	var block: Area2D = block_scene.instantiate()
+
+	block_cluster.add_child(block)
+	$Timer.start()
+
+func _on_area_2d_area_entered(area: Area2D):
+	print('orientation: ' + area.get_meta('side'))
